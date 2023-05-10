5f0a9509d93f30f62a8aa03298dcc5bd58280fbe
Sajedul Islam <65004361+Sajedulislam247@users.noreply.github.com>
2023-05-11 01:27:04 +0600

    Update product.sol

--- contracts/product.sol
+++ contracts/product.sol
@@ -2,19 +2,10 @@
 pragma solidity ^0.8.12;
 
 contract product {
-    uint256 productCount;
     uint256 sellerCount;
+    uint256 productCount;
 
-    struct productItem {
-        uint256 productId;
-        bytes32 productSN;
-        bytes32 productName;
-        bytes32 productBrand;
-        bytes32 productPrice;
-        bytes32 productStatus;
-    }
-    
-    struct seller{
+    struct seller {
         uint256 sellerId;
         bytes32 sellerName;
         bytes32 sellerBrand;
@@ -23,15 +14,29 @@ contract product {
         bytes32 sellerManager;
         bytes32 sellerAddress;
     }
+    mapping(uint256 => seller) public sellers;
+
+    struct productItem {
+        uint256 productId;
+        bytes32 productSN;
+        bytes32 productName;
+        bytes32 productBrand;
+        uint256 productPrice;
+        bytes32 productStatus;
+    }
 
     mapping(uint256 => productItem) public productItems;
-    mapping(uint=>seller) public sellers;
+
+
+
+    //PRODUCT SECTION
 
     function addProduct(
+        //bytes32 _manufactuerID,
         bytes32 _productName,
         bytes32 _productSN,
         bytes32 _productBrand,
-        bytes32 _productPrice
+        uint256 _productPrice
     ) public {
         productItems[productCount] = productItem(
             productCount,
@@ -39,11 +44,38 @@ contract product {
             _productName,
             _productBrand,
             _productPrice,
-           "Available"
+            "Available"
         );
-       
         productCount++;
+    }
 
+    function viewProductItems()
+        public view returns 
+        (
+            uint256[] memory,
+            bytes32[] memory,
+            bytes32[] memory,
+            bytes32[] memory,
+            uint256[] memory,
+            bytes32[] memory
+        )
+    {
+        uint256[] memory pids = new uint256[](productCount);
+        bytes32[] memory pSNs = new bytes32[](productCount);
+        bytes32[] memory pnames = new bytes32[](productCount);
+        bytes32[] memory pbrands = new bytes32[](productCount);
+        uint256[] memory pprices = new uint256[](productCount);
+        bytes32[] memory pstatus = new bytes32[](productCount);
+
+        for (uint256 i = 0; i < productCount; i++) {
+            pids[i] = productItems[i].productId;
+            pSNs[i] = productItems[i].productSN;
+            pnames[i] = productItems[i].productName;
+            pbrands[i] = productItems[i].productBrand;
+            pprices[i] = productItems[i].productPrice;
+            pstatus[i] = productItems[i].productStatus;
+        }
+        return (pids, pSNs, pnames, pbrands, pprices, pstatus);
     }
     
     function addSeller(
